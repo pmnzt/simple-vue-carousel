68439d90bb0554d3c7204e9c5feb3d8aea2eb7a8
pmnzt <vponsve@gmail.com>
2024-09-18 22:33:14 +0300

    update playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -3,7 +3,11 @@ import DemoSimple from './components/demos/Simple.vue';
 </script>
 
 <template>
-  <div>
-    <DemoSimple />
+  <div class="max-w-[90em] mx-auto p-5">
+    <h2 class="text-lg">Demo (Simple)</h2>
+    <hr />
+    <section class="p-5">
+      <DemoSimple />
+    </section>
   </div>
 </template>
