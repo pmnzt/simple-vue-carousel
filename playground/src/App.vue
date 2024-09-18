98a3867acc6d81b30dabd831e0acb5375515a842
pmnzt <vponsve@gmail.com>
2024-09-18 22:54:42 +0300

    update playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import DemoSimple from './components/demos/Simple.vue';
+import DemoClassic from './components/demos/Classic.vue';
 </script>
 
 <template>
@@ -8,6 +9,12 @@ import DemoSimple from './components/demos/Simple.vue';
     <hr />
     <section class="p-5">
       <DemoSimple />
+    </section>
+
+    <h2 class="text-lg">Demo (Classic)</h2>
+    <hr />
+    <section class="p-5">
+      <DemoClassic />
     </section>
   </div>
 </template>
